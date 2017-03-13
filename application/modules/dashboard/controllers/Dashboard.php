ef6524318b11c041157a13c95f566ea1477f302b
Rabin-Lama <rrabin.llama@gmail.com>
2017-03-13 20:15:12 +0545

    dashboard file which was messed up earlier is now recovered

--- application/modules/dashboard/controllers/Dashboard.php
+++ application/modules/dashboard/controllers/Dashboard.php
@@ -1,5 +1,5 @@
 <?php
-	defined('BASEPATH') OR exit(asdasd'No direct script access allowed');
+	defined('BASEPATH') OR exit('No direct script access allowed');
 
 	class Dashboard extends MY_Controller {
 
